547d249cc43f08fc2504b2747c2b62b6f1fec4ce
andres <andresangulo90@gmail.com>
2024-10-22 01:33:29 +0200

    created: router weather

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var weatherRouter = require('./routes/weather');
 
 var app = express();
 const cors = require('cors');
@@ -21,5 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/weather', weatherRouter);
 
 module.exports = app;
--- models/users.js
+++ models/users.js
@@ -8,4 +8,5 @@ const userSchema = mongoose.Schema ({
 })
 
 const User = mongoose.model('users', userSchema);
+
 module.exports = User;
